a04738ea314d73bd25237d85939666291cd61bfe
Santosh Kumar Manda <kumar.santosh581@gmail.com>
2017-05-03 14:58:52 +0530

    Add files via upload
    Added New Solution for NumberOnlyOnceInArray problem with bitwise manipulation.

--- src/org/coding/santosh/NumberOnlyOnceInArray.java
+++ src/org/coding/santosh/NumberOnlyOnceInArray.java
@@ -13,11 +13,13 @@ public class NumberOnlyOnceInArray {
 		// TODO Auto-generated method stub
   int [] arr = {3,3,3,4,4,4,5};
   
-  int count = ReturnValue(arr);
+  int count = ReturnValueBitWise(arr,3);//ReturnValue(arr);
   if(count >= 0)
   System.out.println("The Element Repeating only once is "+count);
   else
 	  System.out.println("No element occurs only once in the given array");
+  //int x = 5 & 1;
+  //System.out.println(x);
 	}
 
 	public static int ReturnValue(int [] arr)
@@ -47,5 +49,31 @@ public class NumberOnlyOnceInArray {
 				return k.intValue();
 		}
 		return -5;
+	}
+	
+	public static int ReturnValueBitWise(int [] arr , int N)
+	{
+		int [] bitarray = new int[32];
+		int bitsum = 0;
+		int uniqueValue = 0;
+		for (int i = 0 ; i < arr.length ; i++)
+		{
+			for(int k = 0 ; k < 32 ; k++)
+			{
+				bitsum = 1 << k ;
+				if((arr[i]&bitsum) == bitsum)
+				{
+					bitarray[k]++;
+				}
+			}
+		}
+		
+		for(int i = 0 ; i < 32 ; i++)
+		{
+			bitarray[i]= bitarray[i] % N;
+			if(bitarray[i] == 1)
+			uniqueValue = uniqueValue | (1 << i);
+		}
+		return uniqueValue;
 	}
 }
