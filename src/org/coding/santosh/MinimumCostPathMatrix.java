ee4e1f9e68b81e1332f11607eb275f97333ddf4f
Santosh Kumar Manda <kumar.santosh581@gmail.com>
2017-05-05 12:46:43 +0530

    To find Minimum cost path from (0,0) to (x,y)
    Given a 2 D matrix with each cell containing cost of traveling that path this program will give 
    minimum cost of traveling from 0,0 to x,y 
    given only 3 movements are possible
    downward right side diagonal by 1 step
    
    http://www.ideserve.co.in/learn/minimum-cost-path

--- src/org/coding/santosh/MinimumCostPathMatrix.java
+++ src/org/coding/santosh/MinimumCostPathMatrix.java
@@ -7,7 +7,7 @@ public class MinimumCostPathMatrix {
 int arr[][] = {
 		{3,2,8},
 		{1,9,7},
-		{0,8,2},
+		{10,8,2},
 		{6,4,3}};
 int sum = MinimumCost(arr,3,2);
 System.out.println(sum);
@@ -32,36 +32,50 @@ System.out.println(sum);
 		{sum = sum + arr[i+1][j+1];i++;j++;
 		return sum;
 		}*/
-			if(i+1 > arr.length-1)
+			if(i == x && j == y)
+			{
+				//sum = sum + arr[i][j];
+				return sum;
+			}
+			
+			if(i+1 == arr.length)
 				minimum = arr[i][j+1];
-			else if(j+1 > arr[0].length-1)
+			else if(j+1 == arr[0].length)
 			minimum = arr[i+1][j];
 			else
 			minimum = Min(arr[i+1][j+1],arr[i+1][j],arr[i][j+1]);
-		if(i == 0 && j == 0 )
-			{sum = sum + arr[i][j];
-			if(arr[i+1][j+1] == minimum)
-			{i++;j++;}
-		else if (arr[i+1][j] == minimum)
-			i++;
-		else
-			j++;
-			}
-		else
-		{
-			sum = sum + minimum +arr[i][j];
-			if( i == x && j == y ) return sum;
-			if(arr[i+1][j+1] == minimum)
-				{i++;j++;}
-			else if (arr[i+1][j] == minimum)
+			
+			if(i == 0 && j == 0 )
+				{sum = sum + arr[i][j] + minimum;
+					if(arr[i+1][j+1] == minimum)
+					{i++;j++;}
+					else if (arr[i+1][j] == minimum)
+					i++;
+					else
+					j++;
+				}
+			else
+			{
+			sum = sum + minimum  ;
+			//if( i == x && j == y ) return sum;
+			if(i+1 == arr.length)
+				j++;
+			else if (j+1 == arr[0].length)
 				i++;
 			else
+			{
+				if(arr[i+1][j+1] == minimum)
+				{i++;j++;}
+				else if (arr[i+1][j] == minimum)
+				i++;
+				else
 				j++;
-		}
+			}
+			}
 			
 		}
-		return sum;
-	}
+			return sum;
+		}	
 	
 	public static int Min(int x ,int y ,int z)
 	{
