f8df0b0ca6dabaf6a0f2303f636b336c7e5a0578
Santosh Kumar Manda <kumar.santosh581@gmail.com>
2017-05-06 14:20:14 +0530

    Adding a variation of Square Island of 1's -- to replace all 1's with positive integers and zeroes and finding island with maximum sum given size of square island as input value

--- src/org/coding/santosh/SquareIslandOfOnes.java
+++ src/org/coding/santosh/SquareIslandOfOnes.java
@@ -1,5 +1,7 @@
 package org.coding.santosh;
 
+import java.util.Scanner;
+
 public class SquareIslandOfOnes {
 
 	public static void main(String[] args) {
@@ -10,9 +12,15 @@ public class SquareIslandOfOnes {
     		{1,1,1,1,1},
     		{1,1,1,1,1},
     		{1,1,1,1,1}};
+    int arr1[][] = 
+    			{{0,5,6},
+    			{4,12,6},
+    			{4,9,7},
+    			{10,9,10}};
+    ;
     int r = arr.length;
     int c = arr[0].length;
-    int p,q, result;
+    int p,q, result, MaxResult;
     int currMax = 2, MaxValue = 2 ,MaxX , MaxY;
     boolean value = false;
     for(int i = 0 ; i < r ; i++)
@@ -61,11 +69,78 @@ public class SquareIslandOfOnes {
    result = maximumSizeOnesSquareDP(arr);
     System.out.println("DP Solution with O(mn) time and O(mn) space is 'maximumSizeOnesSquareDP()' : "+result);
     
+    Scanner s = new Scanner(System.in);
+    int x = s.nextInt();
+    MaxResult =  MaxSumSquareIsland(arr1,x);
+    if(MaxResult > 0 )
+    System.out.println("Maxium Square with "+ x + "*" + x +" dimentions from MaxSumSquareIsland() is  "+MaxResult);
+    else
+    	System.out.println("Not found");
     }
    //boolean x = CheckBorders(arr,2,2,4);
     
    // System.out.println(x);
 	
+	public static int MaxSumSquareIsland(int arr[][] , int m)
+	{
+		if( m < 0 || m > arr.length || m > arr[0].length)
+			return -5;
+		int Maxi = -9 , Maxj = -9, MaxSum = 0;
+		int r = arr.length;
+		int c = arr[0].length;
+		
+		int sumArr[][] = new int [r+1][c+1];
+		int countArr[][] = new int[r+1][c+1];
+		
+		for(int i = 0 ; i < r ; i++)
+		{
+			sumArr[i][0] = arr[i][0];
+			countArr[i][0] = (arr[i][0] == 0 ? 0 : 1);
+		}
+		
+		for(int j = 0 ; j < c ; j++)
+		{
+			sumArr[0][j] = arr[0][j];
+			countArr[0][j] = (arr[0][j] == 0 ? 0 : 1);
+		}
+		
+		for(int i = 1 ; i < r ; i++)
+		{
+			for(int j = 1 ; j < c ; j++)
+			{
+				if(arr[i][j] == 0)
+				{
+					sumArr[i][j] = 0 ;
+					countArr[i][j] = 0;
+				}
+				else
+				{
+					if(arr[i-1][j-1] > 0 && arr[i-1][j] > 0 && arr[i][j-1] > 0)
+					{
+					sumArr[i][j] = arr[i][j]+sumArr[i-1][j-1]+sumArr[i-1][j]+sumArr[i][j-1];
+					}
+					else
+					sumArr[i][j] = arr[i][j];
+					countArr[i][j] = 1 + min(countArr[i-1][j-1],countArr[i][j-1],countArr[i-1][j]);
+				}
+			}
+		}
+		
+		for(int i = 0 ; i < r ; i++)
+		{
+			for(int j = 0 ; j < c ; j++)
+			{
+				if(countArr[i][j] == m && sumArr[i][j] > MaxSum)
+					{
+					Maxi = i;
+					Maxj = j;
+					MaxSum = sumArr[i][j] ;
+					}		
+			}
+		}
+		
+		return (Maxi >=0 && Maxj>=0 ? sumArr[Maxi][Maxj] : -5);
+	}
 	
 	public static boolean baseCase(int arr[][], int i , int j )
 	{
@@ -76,6 +151,7 @@ public class SquareIslandOfOnes {
 		
 	     return false;
 	}
+	
 	
 	public static boolean CheckBorders(int arr[][] , int i , int j , int N)
 	{
