222c2629387d0921cdacc3d9d5e61492113562c5
santoshalex <kumar.santosh581@gmail.com>
2017-05-12 17:35:31 +0530

    Added ArrayList Based Implementation for Rotate Matrix 90 degrees clock wise

--- src/org/coding/santosh/RotateMatrix90Degrees.java
+++ src/org/coding/santosh/RotateMatrix90Degrees.java
@@ -10,6 +10,8 @@ public class RotateMatrix90Degrees {
 				{5,6,7,8},
 				{9,10,11,12},
 				{13,14,15,16}};
+		ArrayList<ArrayList<Integer>> a = new ArrayList<ArrayList<Integer>>();
+		
 		/*int arr[][]={{1,2}
 		,{3,4}};*/
 		/*int arr[][]= 
@@ -62,20 +64,55 @@ public class RotateMatrix90Degrees {
 		}
 	}
 	
-	public void rotate(ArrayList<ArrayList<Integer>> a) {
-    {
-    	
-    }
-     for(ArrayList<Integer> x : a)
-     {
-    	 for(Integer y : x)
-    	 {
-    		
-    	 }
-     }
+	public void rotate(ArrayList<ArrayList<Integer>> arr) {
+    	int start = 0;
+		//int n;
+		Integer t1;//,t4= arr[0][0];
+		int end =  arr.get(0).size()-1;
+		while(start <= end)
+		{
+			//t4 = arr[start][end];
+		//	n = end - start;
+			for(int i = start , k = end; i < end ; i++,k-- )
+			{
+				t1 = arr.get(i).get(end);
+				arr.get(i).set(end,arr.get(start).get(i));
+				arr.get(start).set(i,arr.get(k).get(start));
+				arr.get(k).set(start,arr.get(end).get(k));
+				arr.get(end).set(k, t1);
+				//t4=arr[start][i];
+				
+			}
+			start++;end--;
+		}
     	 
-       
-    }
+		for(int i = 0 ; i < arr.size();i++)
+		{
+			System.out.print("[ ");
+			for(int j = 0 ; j < arr.get(0).size();j++)
+			{
+				
+				System.out.print(arr.get(i).get(j).intValue());
+			//	if(j!= arr.get(0).size()-1)
+			//	System.out.print(", ");
+				
+			}
+			System.out.print(" ]");
+		//	if(i!= arr.size()-1)
+		//	System.out.print(",");
+			//System.out.println();
+		}
+		for(ArrayList<Integer> x : arr)
+		{
+			System.out.print("[");
+			for(Integer k : x)
+			{	
+				System.out.println(k.intValue());
+			}
+			System.out.print(" ");
+			System.out.print("]");
+		}
+
 	/*public static void swap(int arr[][],int i,int j,int k, int l)
 	{
 		int temp;
@@ -83,5 +120,5 @@ public class RotateMatrix90Degrees {
 		arr[i][j]=arr[k][l];
 		//arr[k][l];
 	}*/
-	
+	}
 }
